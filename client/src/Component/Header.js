5a37f8826abade1c4bde658c8f0a5bd0cd293a66
ayushsingh3007 <ayushsingh6394023007@gmil.com>
2024-05-30 22:57:04 -0700

    header

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { VscGlobe } from "react-icons/vsc";
+import { IoSearch } from "react-icons/io5";
+import style from '../Style/header.module.css';
+import { FaSignInAlt } from 'react-icons/fa';
+import { RiAccountCircleLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={style.siteheader}>
+      <nav className={style.navtop}>
+        <div className={style.container}>
+          <span className={style.brandlogo}>1Worldsync</span>
+          <div className={style.searchbar}>
+            <div className={style.hexagon}>
+              <input type="text" placeholder="Search..." className={style.searchinput} />
+              <button className={style.searchbtn}><IoSearch /></button>
+            </div>
+          </div>
+          <VscGlobe className={style.globeicon} />
+          <button className={style.loginbtn}>
+            <FaSignInAlt /> Login
+          </button>
+        
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
