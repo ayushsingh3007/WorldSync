bf0d3c91a78b0ea3707c5eb278d0d181a7bbfe93
Ayush singh <131241505+ayushsingh3007@users.noreply.github.com>
2024-05-31 22:56:36 -0700

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -4,75 +4,9 @@ import style from "../Style/login.module.css"
 
 const Login = () => {
   return (
-    <div className= {style.login_component}>
-        <div className={style.login_banner}>
-            <h1 className={style.login_heading}>LOGIN</h1>
-            <p className= {style.login_slogan}>Choose your product or service below</p>
-        </div>
-        <div className={style.login_product_card_container}>
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-        </div>
-
-        <div className={style.login_product_card_container}>
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-        </div>
-
-        <div className={style.login_product_card_container}>
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-        </div>
-
-        <div className={style.login_product_card_container}>
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-        </div>
-
-        <div className={style.login_product_card_container}>
-            <div className= {style.login_product_card}>
-                <h3 className= {style.login_product_card_heading}>Item Management</h3>
-                <Link to={"/production"} className={style.production}>Production</Link>
-                <Link to={"/pre-production"} className={style.pre_production}>Pre-Production</Link>
-            </div>
-        </div>
-
+    <div >
+        
+       
     </div>
   )
 }
