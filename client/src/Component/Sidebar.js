5d3445eea3bf3ea77c15d53c1ef99f0d976f4e0d
Ayush singh <131241505+ayushsingh3007@users.noreply.github.com>
2024-06-01 02:52:30 -0700

    Update Sidebar.js

--- client/src/Component/Sidebar.js
+++ client/src/Component/Sidebar.js
@@ -5,19 +5,7 @@ import { LuLogOut } from "react-icons/lu";
 
 function Sidebar() {
   return (
-<<<<<<< HEAD
-      <ul className={Style.SidebarContainer}>
-        <li><IoHomeOutline /> Home</li>
-        <li>Leaves</li>
-        <li>Continual</li>
-        <li>Continuation</li>
-        <li>Edit Profile</li>
-        <li>Add Employee
-        </li>
-        <li>Add post </li>
-      </ul>
 
-=======
         <>
         <ul className={Style.SidebarContainer}>
           <li><IoHomeOutline /> Home</li>
@@ -39,7 +27,7 @@ function Sidebar() {
          
         </>
     
->>>>>>> 2243fc356576a5c5da9442e29bd0b7492581663a
+
   );
 }
 
